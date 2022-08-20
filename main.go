d73fa21a0fab2de61e08f92b349cce6842bd0734
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-08-20 15:38:48 +0300

    feat: add hashmap, slice

--- main.go
+++ main.go
@@ -23,20 +23,52 @@ func getPersonalInfo() string {
 	return fullName
 }
 
+func getParents() {
+	const size = 2 // с var компилятор будет ругаться
+	var parents [size] int
+
+	fmt.Println(parents)
+}
+
+func getChildren()  {
+	children := [...]int{1, 2, 3}
+
+	fmt.Println(children)
+}
+
+func getFriends() {
+	var friends [] int
+	fmt.Println(friends)
+
+	friendsNew := make([] int, 0)
+
+	friendsNew = append(friendsNew, 2, 3)
+	fmt.Println("Friends new:", friendsNew)
+
+	copyFriends := make([] int, len(friendsNew), len(friendsNew))
+	fmt.Println("copy friends init:", copyFriends)
+	copy(copyFriends, friendsNew)
+	fmt.Println("copy friends:", copyFriends)
+}
+
+func childHeightInfo() {
+	var person map[string]int = map[string]int{
+		"Vladimir": 170,
+	}
+	person["Kate"] = 153
+	person["Maksim"] = 145
+
+	_, personExists := person["Goga"]
+
+	fmt.Println("Goga exists", personExists);
+}
+
 func main() {
-	var fullName string
-
-	var firstName = "Maksim"
-	var secondName = "Zhuravlev"
-	fullName = firstName + " " + secondName
-	fmt.Println("Full name:", fullName)
-
-	var length = utf8.RuneCountInString(fullName)
-	fmt.Println("Length:", length)
-
-	var slice = fullName[1:3]
-
-	fmt.Println("Slice:", slice)
+	getPersonalInfo()
+	getParents()
+	getChildren()
+	getFriends()
+	childHeightInfo()
 }
 
 
